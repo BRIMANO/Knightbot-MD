2a9f08479842afff057581c5899693d8299714f6
Adebola Oluwaseyi Miracle <99130874+BRIMANO@users.noreply.github.com>
2025-03-16 19:22:53 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,7 +1,7 @@
 const settings = {
   packname: 'Knight',
   author: 'Bot',
-  botName: "Knight Bot",
+  botName: "Brimano Bot",
   botOwner: 'Brimano', // Your name
   ownerNumber: '2348067628624', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
